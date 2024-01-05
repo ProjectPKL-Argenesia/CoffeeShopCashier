5462ded7153695d1b59682208ee8600af2b94837
MuhammadLuthfi14 <muhammadluthfi250201@gmail.com>
2024-01-05 16:21:29 +0700

    memperbarui halaman register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -23,11 +23,21 @@
     <section class="grid w-full h-screen bg-gray-100 place-items-center">
         <div
             class="bg-white rounded-2xl shadow-2xl w-[90vw] lg:w-[80vw] xl:w-[65vw] 2xl:w-[60vw] h-[500px] md:h-[460px] xl:h-[500px] 2xl:h-[600px] grid grid-cols-1 md:grid-cols-2 overflow-hidden">
-            <div class="grid w-full h-full grid-cols-1 px-10 py-4 bg-white">
+            <div class="flex w-full h-full justify-around flex-col gap-y-5 px-10 py-4 bg-white">
                 <div class="text-center">
                     <h1 class="text-2xl font-semibold text-black capitalize">Selamat Datang !</h1>
-                    <p class="text-black/80 text-[15px]">Hello | Silahkan masukan data anda</p>
+                    <p class="text-black/80 text-[15px]">Hello | Silahkan pilih role anda.</p>
                 </div>
+
+                <div class="flex gap-x-2 justify-center items-start">
+                    <div>
+                        <button class="px-3 py-2 bg-blue-500 rounded-lg">Store</button>
+                    </div>
+                    <div>
+                        <button class="px-3 py-2 bg-blue-500 rounded-lg">Cashier</button>
+                    </div>
+                </div>
+
                 <form action="{{ route('register') }}" method="POST">
                     <div>
                         @csrf
@@ -47,7 +57,7 @@
                                     class="w-full rounded-md focus:outline-none focus:ring-0">
                                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
                             </div>
-
+{{-- 
                             <div class="grid grid-cols-1">
                                 <label class="text-base text-black/80" for="password">Password</label>
                                 <input type="password" name="password" id="password" required
@@ -63,21 +73,21 @@
                                     autocomplete="new-password" placeholder="********"
                                     class="w-full rounded-md focus:outline-none focus:ring-0">
                                 <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-                            </div>
+                            </div> --}}
 
                             <div class="grid grid-cols-1">
                                 <button type="submit"
                                     class="bg-[#1F2937] w-full rounded-md text-white py-2 hover:scale-105 hover:bg-gray-900 transition-all duration-300">Daftar</button>
                             </div>
 
-                            <div class="flex items-center justify-between text-xs">
+                            {{-- <div class="flex items-center justify-between text-xs">
                                 <p class="text-gray-800">kamu sudah punya akun ?</p>
                                 @if (Route::has('register'))
                                     <a class="py-2 px-5 bg-white border text-[#000000] rounded-md shadow-md hover:scale-110 duration-300"
                                         href="{{ route('login') }}">Masuk
                                     </a>
                                 @endif
-                            </div>
+                            </div> --}}
                         </div>
                     </div>
                 </form>
